82ee1d75f43577128528141a1e73c6585328792d
Bé Sữa <hoangluuminh1@gmail.com>
2024-02-21 19:04:03 +0700

    Main UI Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
+    implementation ("androidx.cardview:cardview:1.0.0")
 }
