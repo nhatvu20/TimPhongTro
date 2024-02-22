4653ca388b6d70f9b828e59cc4bf0f8f447cbb3e
Bé Sữa <hoangluuminh1@gmail.com>
2024-02-23 00:28:54 +0700

    Main UI Updated

--- app/src/main/java/com/example/timphongtro/HomeFragment.java
+++ app/src/main/java/com/example/timphongtro/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +19,6 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -38,4 +39,5 @@ public class HomeFragment extends Fragment {
 
         imageSlider.setImageList(slideModels, ScaleTypes.FIT);
     }
+
 }
