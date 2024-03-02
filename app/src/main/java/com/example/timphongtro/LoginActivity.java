79be1c67ab3e04d19d1913187cc10574a20a5d70
Huyhoang1128 <phamhuyhoang281103@gmail.com>
2024-03-02 21:48:05 +0700

    Update login

--- app/src/main/java/com/example/timphongtro/LoginActivity.java
+++ app/src/main/java/com/example/timphongtro/LoginActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -19,30 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class SplashScreen extends AppCompatActivity {
-
-//    public static int SPLASH_TIMER = 3000;
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
-
- //       setContentView(R.layout.activity_splash_screen);
-
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        }, SPLASH_TIMER);
-
-//    }
-
-
-
+public class LoginActivity extends AppCompatActivity {
     private EditText txtSDT;
     private Button btnDangky, btnDangnhap;
     private FirebaseAuth mAuth;
@@ -75,7 +51,7 @@ public class SplashScreen extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(getApplicationContext(), "Dang nhap thanh cong", LENGTH_SHORT).show();
-                            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(i);
                         } else {
                             Toast.makeText(getApplicationContext(), "Dang nhap khong thanh cong", LENGTH_SHORT).show();
@@ -92,9 +68,10 @@ public class SplashScreen extends AppCompatActivity {
             }
 
             private void Dangky() {
-                Intent i = new Intent(SplashScreen.this, RegisterActivity.class);
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(i);
             }
         });
     }
+
 }
