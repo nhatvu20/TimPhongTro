cf671c3cf073820fedb8f9a4296c0359b9637aab
Huyhoang1128 <phamhuyhoang281103@gmail.com>
2024-04-06 23:30:52 +0700

    Thêm isEmpty

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:15.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
