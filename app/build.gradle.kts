fda3400168cae497014201bbd6dd31d3beb9bdfc
Bé Sữa <hoangluuminh1@gmail.com>
2024-04-10 22:08:13 +0700

    Fix History

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:15.0")
+    implementation("org.jetbrains:annotations:23.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/timphongtro/Adapter/SearchAdapter.java
+++ app/src/main/java/com/example/timphongtro/Adapter/SearchAdapter.java
@@ -18,13 +18,24 @@ import com.example.timphongtro.Entity.Address;
 import com.example.timphongtro.Entity.ImagesRoomClass;
 import com.example.timphongtro.Entity.Room;
 import com.example.timphongtro.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHolder> {
     Context context;
     ArrayList<Room> list;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser user = firebaseAuth.getCurrentUser();
+    Room room;
     public SearchAdapter(Context context, ArrayList<Room> list) {
         this.context = context;
         this.list = list;
@@ -57,13 +68,36 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHold
         holder.cardViewRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String userID = "";
+                if(user != null) {
+                    userID = user.getUid();
+                }
                 Intent detailRoom = new Intent(context, DetailRoomActivity.class);
                 detailRoom.putExtra("DataRoom", room.toString());
                 context.startActivity(detailRoom);
+                RecentlyRead(userID,holder);
             }
         });
     }
 
+    private void RecentlyRead(String userID, SearchAdapter.MyViewHolder holder) {
+        if(user != null) {
+
+            room = list.get(holder.getAdapterPosition());
+            Date timeRead = new Date();
+            long timestamp = timeRead.getTime();
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("History/" + userID);
+            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    databaseReference.child(room.getId_room()).setValue(timestamp);
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                }
+            });
+        }
+    }
     @Override
     public int getItemCount() {
         return list.size();
